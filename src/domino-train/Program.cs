56e6a7f5294e53474516b8aada24e154fb3c2a04
Jessica Mulein <jessica@digitaldefiance.org>
2022-05-15 13:01:33 -0700

    use new extension

--- src/domino-train/Program.cs
+++ src/domino-train/Program.cs
@@ -5,33 +5,13 @@ using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Identity.Firebase.Components;
+using static Microsoft.Identity.Firebase.Extensions.FirebaseAuthExtensions;
 using Microsoft.Identity.Firebase.Models;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-var httpClient = new HttpClient {
-    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
-};
-var openIdConfiguration = await FirebaseOpenIdConfiguration.GetFirebaseOpenIdConfigurationAsync(
-        configurationUrl: string.Join(httpClient.BaseAddress.ToString(), "/openIdConfiguration.json"),
-        httpClient: httpClient);
-var firebaseConfiguration = new FirebaseProjectConfiguration(builder.Configuration, openIdConfiguration);
-var firebaseAuth = new FirebaseAuth();
-FirebaseAuth.SetFirebaseConfiguration(firebaseConfiguration);
-
-builder.Services.AddOidcAuthentication(options =>
-{
-    builder.Configuration.Bind("Local", options.ProviderOptions);
-});
-builder.Services.AddScoped(sp => httpClient);
-builder.Services.AddSingleton<FirebaseOpenIdConfiguration>(implementationInstance: openIdConfiguration);
-builder.Services.AddSingleton<FirebaseProjectConfiguration>(implementationInstance: firebaseConfiguration);
-builder.Services.AddSingleton<FirebaseAuth>(implementationInstance: firebaseAuth);
-builder.Services.AddSingleton<StateProvider>();
-builder.Services.AddScoped<IdentityUser, FirebaseUser>();
-builder.Services.AddSingleton<AuthenticationStateProvider>(s => s.GetRequiredService<StateProvider>());
-builder.Services.AddSingleton<SignOutSessionStateManager, FirebaseSignOutSessionStateManager>();
+await builder.UseFirebaseAuthenticationAsync();
 
 await builder.Build().RunAsync();
