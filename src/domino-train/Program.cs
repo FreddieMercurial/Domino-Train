03255c41bf814eba084548e68a67ec512690309e
Freddie Mercurial (Jessica M.) <jessica@brightchain.org>
2022-06-04 23:32:33 -0700

    try to do presence

--- src/domino-train/Program.cs
+++ src/domino-train/Program.cs
@@ -6,7 +6,5 @@ using static Microsoft.Identity.Firebase.Extensions.FirebaseAuthExtensions;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
 await builder.UseFirebaseAuthenticationAsync();
-
 await builder.Build().RunAsync();
